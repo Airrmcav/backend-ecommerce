88e8ef5bf23d483260c076b202a08587d38c730a
Axel Ramirez <ivanramres@gmail.com>
2026-04-02 11:50:55 -0600

    Reset

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,4 @@
-export default ({ env }) => ({
-  // --- Tu configuración de Cloudinary (Ya la tienes) ---
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
-
-  // --- NUEVA CONFIGURACIÓN DE EMAIL (Agrega esto) ---
+module.exports = ({ env }) => ({
   email: {
     config: {
       provider: 'nodemailer',
@@ -27,7 +9,7 @@ export default ({ env }) => ({
           user: env('SMTP_USER'),
           pass: env('SMTP_PASS'),
         },
-        secure: true, // Forzamos true porque Titan usa SSL en el 465
+        secure: true, // Titan usa SSL en el puerto 465
       },
       settings: {
         defaultFrom: env('SMTP_FROM'),
